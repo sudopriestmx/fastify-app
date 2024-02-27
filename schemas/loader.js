9a17f479b72ad5efea7cdf0cf97aae2dfa2e750c
Jose Pablo Ortiz Lack <josepablo.ortiz@naranja.com>
2024-02-26 22:17:46 -0600

    added global limit and skip schemas

--- schemas/loader.js
+++ schemas/loader.js
@@ -1,6 +1,7 @@
 const fp = require('fastify-plugin')
 module.exports = fp(function (fastify, opts, next) {
   fastify.addSchema(require('./dotenv.json'))
-  //fastify.addSchema(require('./user-input-headers.json'))
+  fastify.addSchema(require('./limit.json'))
+  fastify.addSchema(require('./skip.json'))
   next()
 })
