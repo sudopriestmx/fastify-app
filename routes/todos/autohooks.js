221218f5a2cb650bd3290a43ecca8617cebfe7d0
Jose Pablo Ortiz Lack <josepablo.ortiz@naranja.com>
2024-03-04 15:26:49 -0600

    added userId logic to to-do dataSource

--- routes/todos/autohooks.js
+++ routes/todos/autohooks.js
@@ -3,66 +3,73 @@ const schemas = require('./schemas/loader')
 module.exports = fp(async function todoAutoHooks (fastify, opts) {
     const todos = fastify.mongo.db.collection('todos')
     fastify.register(schemas)
-    fastify.decorate('mongoDataSource', {
-        async countTodos (filter = {}) {
-            const totalCount = await todos.countDocuments(filter)
-            return totalCount
-        },
-        async listTodos ({
-            filter = {},
-            projection = {},
-            skip = 0,
-            limit = 50
-        } = {}) {
-            if (filter.title) {
-                filter.title = new RegExp(filter.title, 'i')
-            } else {
-                delete filter.title
-            }
-            const todoDocuments = await todos
-                .find(filter, {
-                    projection: { ...projection, _id: 0},
-                    limit,
-                    skip
-                })
-                .toArray()
-            return todoDocuments
-        },
-        async createTodo ({title}) {
-            const _id = new fastify.mongo.ObjectId()
-            const now = new Date()
-            const { insertedId } = await todos.insertOne({
-                _id,
-                title,
-                done: false,
-                id: _id,
-                createdAt: now,
-                modifiedAt: now
-            })
-            return instertedId
-        },
-        async readTodo (id, projection = {}) {
-            const todo = await todos.findOne(
-                { _id: new fastify.mongo.ObjectId(id) },
-                { projection: {...projection, _id: 0 } }
-            )
-            return todo
-        },
-        async updateTodo (id, newTodo) {
-            return todos.updateOne(
-                { _id: new fastify.mongo.ObjectId(id) },
-                {
-                    $set: {
-                        ...newTodo,
-                        modifiedAt: new Date()
-                    }
+    fastify.decorate('todosDataSource', null)
+    fastify.addHook('onRequest', async (request, reply) => {
+        request.todosDataSource = {
+            async countTodos (filter = {}) {
+                const totalCount = await todos.countDocuments(filter)
+                return totalCount
+            },
+            async listTodos ({
+                filter = {},
+                projection = {},
+                skip = 0,
+                limit = 50
+            } = {}) {
+                if (filter.title) {
+                    filter.title = new RegExp(filter.title, 'i')
+                } else {
+                    delete filter.title
                 }
-            )
-        },
-        async deleteTodo (id) {
-            return todos.deleteOne({ _id: new fastify.mongo.ObjectId(id)})
+                filter.userId = request.user.id
+                const todoDocuments = await todos
+                    .find(filter, {
+                        projection: { ...projection, _id: 0},
+                        limit,
+                        skip
+                    })
+                    .toArray()
+                return todoDocuments
+            },
+            async createTodo ({title}) {
+                const _id = new fastify.mongo.ObjectId()
+                const now = new Date()
+                const userId = request.user.id
+                const { insertedId } = await todos.insertOne({
+                    _id,
+                    userId,
+                    title,
+                    done: false,
+                    id: _id,
+                    createdAt: now,
+                    modifiedAt: now
+                })
+                return instertedId
+            },
+            async readTodo (id, projection = {}) {
+                const todo = await todos.findOne(
+                    { _id: new fastify.mongo.ObjectId(id), userId: request.user.id },
+                    { projection: {...projection, _id: 0 } }
+                )
+                return todo
+            },
+            async updateTodo (id, newTodo) {
+                return todos.updateOne(
+                    { _id: new fastify.mongo.ObjectId(id), userId: request.user.id },
+                    {
+                        $set: {
+                            ...newTodo,
+                            modifiedAt: new Date()
+                        }
+                    }
+                )
+            },
+            async deleteTodo (id) {
+                return todos.deleteOne({ _id: new fastify.mongo.ObjectId(id), userId: request.user.id})
+            }
         }
     })
+    fastify.decorate('todosDataSource', )
 }, {
     encapsulate: true,
     dependencies: ['@fastify/mongodb'],
